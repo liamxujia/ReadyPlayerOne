abc787670bf90478e2cfc577c74170601cc2a17b
liam <liamxujia@hotmail.com>
2020-08-03 11:11:58 +0800

    [pod]+ARKit

--- Example/Pods/Target Support Files/Pods-ReadyPlayerOne_Example/Pods-ReadyPlayerOne_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ReadyPlayerOne_Example/Pods-ReadyPlayerOne_Example-frameworks.sh
@@ -197,9 +197,13 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ARCL/ARCL.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ARVideoKit/ARVideoKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReadyPlayerOne/ReadyPlayerOne.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ARCL/ARCL.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ARVideoKit/ARVideoKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReadyPlayerOne/ReadyPlayerOne.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- ReadyPlayerOne.podspec
+++ ReadyPlayerOne.podspec
@@ -28,7 +28,7 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/liamxujia@hotmail.com/ReadyPlayerOne.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '13.0'
 
   s.source_files = 'ReadyPlayerOne/Classes/**/*'
   
@@ -38,5 +38,6 @@ TODO: Add long description of the pod here.
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'ARCL'
+  s.dependency 'ARVideoKit'
 end
